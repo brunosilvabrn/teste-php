04a5b7ea1a9f6ddf2aebf318ad37c78f7d6eeb2f
Bruno Lopes Silva <brunolopessilva34@gmail.com>
2022-03-09 18:02:34 -0300

    add script

---
+++ sequencia.php
@@ -0,0 +1,42 @@
+<?php
+
+
+function SequenciaCrescente($array) {
+
+	$chances = 0;
+
+	for ($i = 0; $i < count($array); $i++) { 
+		$verificar = $array[$i];
+		for ($j = 0; $j < count($array); $j++) { 
+			if($verificar == $array[$j] && $i > $j) {
+				$chances++;
+				break;
+			}
+		}
+	}
+
+	$array = array_unique($array);
+	$arr = [];
+
+	foreach ($array as $value) {
+		array_push($arr, $value);
+	}
+
+
+	for ($i = 0; $i < count($array) - 1; $i++) { 
+		$numeroAtual = $arr[$i];
+		for ($j = 0; $j < count($arr); $j++) {
+			if ($numeroAtual > $arr[$j] && $j > $i || $numeroAtual == $arr[$j] && $j > $i) {
+				$chances++;
+				break;
+			}
+		}
+
+	}
+
+	if($chances < 2) {
+		return true;
+	}
+
+	return false;
+}
